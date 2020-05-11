2fcc8b540988b2544c1163e48aa2c32571c45e14
Nathan Ostrowski <nathancostrowski@gmail.com>
2020-05-11 19:49:57 -0400

    Initial Commit

---
+++ MuseumViewerTests/MuseumViewerTests.swift
@@ -0,0 +1,34 @@
+//
+//  MuseumViewerTests.swift
+//  MuseumViewerTests
+//
+//  Created by Nathan Ostrowski on 5/11/20.
+//  Copyright © 2020 Nathan Ostrowski. All rights reserved.
+//
+
+import XCTest
+@testable import MuseumViewer
+
+class MuseumViewerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
